40030a7ecdf1d224f6220c24f9afb169c4ab621f
sheikh-muzammil2026 <discordsstudent@gmail.com>
2026-06-24 05:57:11 +0600

    fixed problem of git pushing , added stripe api, deleted secret key

--- src/app/dashboard/tenant/profile/page.jsx
+++ src/app/dashboard/tenant/profile/page.jsx
@@ -1,6 +1,7 @@
 import Profile from '@/components/dashboard/tenant/profile';
 import UserProfile from '@/components/shared/userProfile';
 import React from 'react';
+import { getUserSession } from '@/lib/core/session';
 
 const ProfilePage = async () => {
     const user = await getUserSession()
