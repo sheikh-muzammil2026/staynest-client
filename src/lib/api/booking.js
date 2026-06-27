c4a6423f28c7ac764307715176163fad6ea57dd2
Sheikh Muzammil <discordsstudent@gmail.com>
2026-06-27 16:08:02 +0600

    Added update booking status client booking.js

--- src/lib/api/booking.js
+++ src/lib/api/booking.js
@@ -30,3 +30,14 @@ export const getTenantBookings = async (tenantEmail) => {
     return data;
 }
 
+export const updateBookingStatus = async (id, status) => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URI}/bookings/status`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ id, status }),
+    });
+    if (!res.ok) throw new Error("Failed to update booking status");
+    return res.json();
+};
